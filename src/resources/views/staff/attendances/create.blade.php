ce847076fed9c493076e448b5b0a7659a96c5244
tamori <k.m.biz169@gmail.com>
2026-06-03 10:36:40 +0900

    勤怠登録画面スタイル完了

--- src/resources/views/staff/attendances/create.blade.php
+++ src/resources/views/staff/attendances/create.blade.php
@@ -16,13 +16,12 @@
         </div>
         <!-- 現在時刻 -->
         <div class="current-time">
+            @php
+            $now = now('Asia/Tokyo');
+            $weekdays = ['日', '月', '火', '水', '木', '金', '土'];
+            @endphp
             <span class="current-time__date" id="current-date">
-                @php
-                $now = now('Asia/Tokyo');
-                $weekdays = ['日', '月', '火', '水', '木', '金', '土'];
-                @endphp
-
-                {{ $now->format('Y年n月j日') }}（{{ $weekdays[$now->dayOfWeek] }}）
+                {{ $now->format('Y年n月j日') }}({{ $weekdays[$now->dayOfWeek] }})
             </span>
             <span class="current-time__time" id="current-time">
                 {{ now('Asia/Tokyo')->format('H:i') }}
@@ -35,15 +34,17 @@
             <button class="attendance-buttons__clock_in" type="submit">出勤</button>
         </form>
         @elseif(auth()->user()->attendance_status === '出勤中')
-        <form class="attendance-buttons" method="post">
-            @csrf
-            @method('PATCH')
-            <button class="attendance-buttons__clock_out" formaction="{{ route('staff.attendances.clockOut') }}" type="submit">退勤</button>
-        </form>
-        <form class="attendance-buttons" method="post">
-            @csrf
-            <button class="attendance-buttons__break_in" formaction="{{ route('staff.attendances.breakIn') }}" type="submit">休憩入</button>
-        </form>
+        <div class="attendance-buttons__wrapper">
+            <form class="attendance-buttons" method="post">
+                @csrf
+                @method('PATCH')
+                <button class="attendance-buttons__clock_out" formaction="{{ route('staff.attendances.clockOut') }}" type="submit">退勤</button>
+            </form>
+            <form class="attendance-buttons" method="post">
+                @csrf
+                <button class="attendance-buttons__break_in" formaction="{{ route('staff.attendances.breakIn') }}" type="submit">休憩入</button>
+            </form>
+        </div>
         @elseif(auth()->user()->attendance_status === '休憩中')
         <form class="attendance-buttons" method="post">
             @csrf
