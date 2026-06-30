89c1a19d270feb56ec5a525493ddc0cc0de21b5d
tamori <k.m.biz169@gmail.com>
2026-06-30 14:20:09 +0900

    FormRequest記述

--- src/app/Http/Requests/Api/V1/IndexAttendanceRecordRequest.php
+++ src/app/Http/Requests/Api/V1/IndexAttendanceRecordRequest.php
@@ -7,24 +7,45 @@ use Illuminate\Foundation\Http\FormRequest;
 class IndexAttendanceRecordRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
+     * リクエストの実行を許可するか判定する。
      *
      * @return bool
      */
-    public function authorize()
+    public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
-     * Get the validation rules that apply to the request.
+     * バリデーションルールを取得。
      *
      * @return array
      */
-    public function rules()
+    public function rules(): array
     {
         return [
-            //
+            'user_id' => ['nullable', 'integer'],
+            'date' => ['nullable', 'date_format:Y-m-d'],
+            'month' => ['nullable', 'date_format:Y-m'],
+            'page' => ['nullable', 'integer'],
+            'per_page' => ['nullable', 'integer', 'max:100'],
+        ];
+    }
+
+    /**
+     * バリデーションエラーメッセージを取得する。
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'user_id.integer' => 'ユーザーIDは数値で指定してください。',
+            'date.date_format' => '勤怠日は YYYY-MM-DD 形式で指定してください。',
+            'month.date_format' => '勤怠日は YYYY-MM 形式で指定してください。',
+            'page.integer' => 'ページ数は数値で指定してください。',
+            'per_page.integer' => '1ページあたりの件数は数値で指定してください。',
+            'per_page.max' => '1ページあたりの件数は100以内で指定してください。'
         ];
     }
 }
